345c03e8d3957c52dbccd13af6f9bcbe9081137a
Lawrence <144321399+Hsuan04@users.noreply.github.com>
2023-10-16 13:29:03 +0000

    feat: delete useless folders

--- src/main/java/com/tha103/artion/merchorderdetail/model/MerchOrderDetailDAO_interface.java
+++
@@ -1,5 +0,0 @@
-package com.tha103.artion.merchOrderDetail.model;
-
-public class MerchOrderDetailDAO_interface {
-
-}
--- src/main/java/com/tha103/artion/ticketorder/model/TicketOrderDAO.java
+++
@@ -1,5 +0,0 @@
-package com.tha103.artion.ticketOrder.model;
-
-public class TicketOrderDAO {
-
-}
